d4a31f7f1542da48af882e90ab906a132de00ecb
ninolis <97913013+ninolis@users.noreply.github.com>
2022-11-30 21:56:15 +0200

    update task1, task2

--- js/task-02.js
+++ js/task-02.js
@@ -9,43 +9,56 @@ const ingredients = [
 
 const ul = document.getElementById('ingredients');
 
-///////////////////////////////////////////////////////
+/////////////////////////////////////////
 // ** variant 1
+
+const liList = [];
+for (const ingredient of ingredients) {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.setAttribute('class', 'item');
+  liList.push(li);
+}
+
+ul.append(...liList);
+
+///////////////////////////////////////////////////////
+// ** variant 2
 
 // for (const ingredient of ingredients) {
 //   const li = document.createElement('li');
 //   li.textContent = ingredient;
 //   li.setAttribute('class', 'item');
-//   ul.appendChild(li);
+//   ul.append(li);
 // }
 
 ///////////////////////////////////////////////////////
-// ** variant 2
+// ** variant 3
 
 // for (let i = 0; i < ingredients.length; i += 1) {
 //   const li = document.createElement('li');
 //   li.textContent = ingredients[i];
 //   li.setAttribute('class', 'item');
-//   ul.appendChild(li);
+//   ul.append(li);
 // }
 
 /////////////////////////////////////////
-// ** variant 3
+// ** variant 4
 
-const allLi = [];
-for (const ingredient of ingredients) {
-  const li = document.createElement('li');
-  li.textContent = ingredient;
-  li.setAttribute('class', 'item');
-  allLi.push(li);
-}
+// const allLi = [];
+// for (const ingredient of ingredients) {
+//   const li = document.createElement('li');
+//   li.textContent = ingredient;
+//   li.setAttribute('class', 'item');
+//   allLi.push(li);
+// }
 
-allLi.forEach((li) => {
-  ul.appendChild(li);
-});
+// allLi.forEach((li) => {
+//   ul.appendChild(li);
+// });
 
 /////////////////////////////////////////
-// ** variant 4
+// ** variant 5
 
 // let htmlString = '';
 
