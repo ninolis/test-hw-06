540f9cf3643bafb474696482b1d6f9977d643e2d
ninolis <97913013+ninolis@users.noreply.github.com>
2022-12-03 22:30:23 +0200

    lesson-12-form-submit

---
+++ js/lesson-12-form-submit.js
@@ -0,0 +1,19 @@
+'use strict';
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { login, password },
+  } = event.currentTarget;
+
+  if (login.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log(`Login: ${login.value}, Password: ${password.value}`);
+  alert(`Login: ${login.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
