ef6d2f37a8df1d60d98f3287120e8caf838147e2
ninolis <97913013+ninolis@users.noreply.github.com>
2022-12-03 23:24:14 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const buttonChangeColor = document.querySelector('.change-color');
+const spanColorValue = document.querySelector('.color');
+
+buttonChangeColor.addEventListener('click', changeColor);
+
+function changeColor() {
+  const randomHexColor = getRandomHexColor();
+  // document.body.style.backgroundColor = randomHexColor;
+  document.body.style.cssText = `background-color: ${randomHexColor}`;
+
+  spanColorValue.textContent = randomHexColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
